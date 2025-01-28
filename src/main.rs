e35a0be5837e7c61977387a96a58deaaa16ff7ad
Aiden Voth <aiden@aidens-pc.localdomain>
2025-01-28 18:37:53 -0500

    gitignore and write first endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use sqlx::SqlitePool;
+use tokio::net::TcpListener;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tracing_subscriber::EnvFilter;
+
+mod refs;
+
+#[tokio::main]
+async fn main() {
+    let sqlite_addr = dotenv::var("DATABASE_URL").unwrap();
+
+    let conn_pool = SqlitePool::connect(&sqlite_addr).await.unwrap();
+
+    sqlx::query!("CREATE TABLE IF NOT EXISTS refs (refstr TEXT, name TEXT, relevant_skills BLOB)")
+        .execute(&conn_pool)
+        .await
+        .unwrap();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new(dotenv::var("LOG_LEVEL").unwrap()))
+        .init();
+
+    let app = Router::new()
+        .route("/ref/{ref}/name", get(refs::get_ref_name))
+        .route("/ref/create", get(refs::create_ref))
+        .layer(CorsLayer::very_permissive())
+        .layer(TraceLayer::new_for_http())
+        .with_state(conn_pool);
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
---
+++ src/refs.rs
@@ -0,0 +1,80 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use base64::Engine;
+use serde::{Deserialize, Serialize};
+use sqlx::{query, query_as, SqlitePool};
+use tracing::instrument;
+
+#[derive(Debug, Serialize, Deserialize, Hash)]
+pub struct EntityReference {
+    name: String,
+    skills: Vec<String>,
+}
+
+struct Count {
+    count: i64,
+}
+
+#[instrument(skip(pool) err(Debug))]
+pub async fn create_ref(
+    State(pool): State<SqlitePool>,
+    Json(reference): Json<EntityReference>,
+) -> Result<Response, Response> {
+    let mut h = DefaultHasher::default();
+    reference.hash(&mut h);
+
+    let ref_key = base64::engine::general_purpose::URL_SAFE.encode(h.finish().to_be_bytes());
+
+    let Count { count } = query_as!(
+        Count,
+        "SELECT COUNT(*) as count FROM refs WHERE refstr = ?",
+        ref_key
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    if count != 0 {
+        return Ok((StatusCode::BAD_REQUEST, "Reference already exists").into_response());
+    }
+
+    let skills_json = serde_json::to_string(&reference.skills).unwrap();
+
+    query!(
+        "INSERT INTO refs (refstr, name, relevant_skills) VALUES(?, ?, ?)",
+        ref_key,
+        reference.name,
+        skills_json
+    )
+    .execute(&pool)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    Ok((StatusCode::OK, ref_key).into_response())
+}
+
+#[instrument(skip(pool) err(Debug))]
+pub async fn get_ref_name(
+    State(pool): State<SqlitePool>,
+    Path(ref_str): Path<String>,
+) -> Result<Response, Response> {
+    let result = query!("SELECT name FROM refs WHERE refstr = ?", ref_str)
+        .fetch_one(&pool)
+        .await;
+
+    match result {
+        Ok(rec) => Ok((
+            StatusCode::OK,
+            rec.name
+                .ok_or(StatusCode::INTERNAL_SERVER_ERROR.into_response())?,
+        )
+            .into_response()),
+        Err(_) => Ok((StatusCode::BAD_REQUEST, "Ref does not exist").into_response()),
+    }
+}
