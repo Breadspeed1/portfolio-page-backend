af69e94aa02cf4a32c6444f7648a31a3ea9f33cc
Aiden Voth <aiden@aidens-pc.localdomain>
2025-01-28 21:08:15 -0500

    endpoints done..?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use sqlx::SqlitePool;
 use tokio::net::TcpListener;
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
@@ -12,10 +12,9 @@ async fn main() {
 
     let conn_pool = SqlitePool::connect(&sqlite_addr).await.unwrap();
 
-    sqlx::query!("CREATE TABLE IF NOT EXISTS refs (refstr TEXT, name TEXT, relevant_skills BLOB)")
-        .execute(&conn_pool)
-        .await
-        .unwrap();
+    sqlx::migrate!("./migrations")
+        .run(&conn_pool)
+        .await.unwrap();
 
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::new(dotenv::var("LOG_LEVEL").unwrap()))
@@ -23,7 +22,11 @@ async fn main() {
 
     let app = Router::new()
         .route("/ref/{ref}/name", get(refs::get_ref_name))
-        .route("/ref/create/{name}", get(refs::create_ref))
+        .route("/ref/create/{name}", post(refs::create_ref))
+        .route("/skills/create/{name}", post(refs::create_skill))
+        .route("/skills/search/{search_term}", get(refs::search_skills))
+        .route("/ref/{ref}/add_skill/{skill}", post(refs::add_skill_to_ref))
+        .route("/ref/{ref}/skills", get(refs::get_skills))
         .layer(CorsLayer::very_permissive())
         .layer(TraceLayer::new_for_http())
         .with_state(conn_pool);
--- src/refs.rs
+++ src/refs.rs
@@ -1,4 +1,4 @@
-use std::hash::{DefaultHasher, Hash, Hasher};
+use std::{cmp::Ordering, hash::{DefaultHasher, Hash, Hasher}};
 
 use axum::{
     extract::{Path, State},
@@ -47,7 +47,7 @@ pub async fn create_ref(
         "INSERT INTO refs (refstr, name, relevant_skills) VALUES(?, ?, ?)",
         ref_key,
         name,
-        ""
+        "[]"
     )
     .execute(&pool)
     .await
@@ -74,4 +74,93 @@ pub async fn get_ref_name(
             .into_response()),
         Err(_) => Ok((StatusCode::BAD_REQUEST, "Ref does not exist").into_response()),
     }
+}
+
+pub async fn create_skill(State(pool): State<SqlitePool>, Path(name): Path<String>) -> Result<Response, Response> {
+    let Count { count } = query_as!(
+        Count,
+        "SELECT COUNT(*) as count FROM skills WHERE skill = ?",
+        name
+    ).fetch_one(&pool).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    if count > 0 {
+        return Ok((StatusCode::BAD_REQUEST, "Skill already exists").into_response());
+    }
+
+    query!(
+        "INSERT INTO skills (skill) VALUES(?)",
+        name
+    ).execute(&pool).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    Ok(StatusCode::OK.into_response())
+}
+
+pub async fn search_skills(State(pool): State<SqlitePool>, Path(search_term): Path<String>) -> Result<Response, Response> {
+    let skills: Vec<String> = query!("SELECT skill FROM skills")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?
+        .into_iter()
+        .map(|row| row.skill.unwrap())
+        .collect();
+
+    let skills: Vec<&str> = skills.iter().map(|s| s.as_ref()).collect();
+
+    let mut res = rust_fuzzy_search::fuzzy_search_threshold(&search_term, skills.as_slice(), 0.5);
+
+    //bad solution
+    res.sort_by(|(_, v1), (_, v2)| v1.partial_cmp(v2).unwrap_or(Ordering::Equal));
+
+    let res = res.iter().map(|(s, _)| s.to_string()).collect::<Vec<String>>();
+    let res = serde_json::ser::to_string(&res).unwrap();
+
+    Ok((StatusCode::OK, res).into_response())
+}
+
+pub async fn add_skill_to_ref(State(pool): State<SqlitePool>, Path((refstr, skill)): Path<(String, String)>) -> Result<Response, Response> {
+    let Count { count } = query_as!(
+        Count,
+        "SELECT COUNT(*) as count FROM skills WHERE skill = ?",
+        skill
+    ).fetch_one(&pool).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    if count == 0 {
+        return Err((StatusCode::BAD_REQUEST, "Skill does not exist").into_response());
+    }
+
+    let mut tx = pool.begin().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    let skills_data = query!(
+        "SELECT relevant_skills FROM refs WHERE refstr = ?",
+        refstr
+    ).fetch_optional(&mut *tx).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?
+    .ok_or((StatusCode::BAD_REQUEST, "Reference does not exist").into_response())?
+    .relevant_skills.unwrap();
+
+    let mut skills: Vec<String> = serde_json::de::from_slice(skills_data.as_slice()).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    skills.push(skill);
+
+    let skills_str = serde_json::ser::to_string(&skills).unwrap();
+
+    query!(
+        "UPDATE refs SET relevant_skills = ? WHERE refstr = ?",
+        skills_str,
+        refstr
+    ).execute(&mut *tx).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    tx.commit().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?;
+
+    Ok(StatusCode::OK.into_response())
+}
+
+pub async fn get_skills(State(pool): State<SqlitePool>, Path(refstr): Path<String>) -> Result<Response, Response> {
+    let skills_data = query!(
+        "SELECT relevant_skills FROM refs WHERE refstr = ?",
+        refstr
+    ).fetch_optional(&pool).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR.into_response())?
+    .ok_or((StatusCode::BAD_REQUEST, "Ref does not exist").into_response())?
+    .relevant_skills.unwrap();
+
+    Ok((StatusCode::OK, String::from_utf8(skills_data).unwrap()).into_response())
 }
